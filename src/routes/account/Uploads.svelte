329609a11f125fc4745c15875f017d03e411bc80
akolt19d <a.kolt.19d@gmail.com>
2023-08-12 23:32:28 +0200

    Added Uploads page

--- src/routes/account/Uploads.svelte
+++ src/routes/account/Uploads.svelte
@@ -1,1 +1,18 @@
-<p>Uploads</p>
+<script>
+    import PreviewCard from "$lib/components/PreviewCard.svelte"
+
+    const card = {
+        title: "Title",
+        description: "Description Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente quasi repellat in officiis nulla cumque iusto asperiores a aspernatur at.",
+        date: "1 year ago",
+        rating: "4,5"
+    }
+</script>
+
+<h1 class="h1 px-4">Your uploads</h1>
+<hr class="my-6">
+<div class="flex flex-wrap gap-8 px-6 justify-center">
+    {#each Array(8) as _, index (index)}
+        <PreviewCard { ...card }  />
+    {/each}
+</div>
