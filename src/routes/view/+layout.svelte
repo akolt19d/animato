57934291b2f106f8088deef2eca8b1de1e6fce20
akolt19d <a.kolt.19d@gmail.com>
2023-08-25 22:49:39 +0200

    Added routes for viewing content

---
+++ src/routes/view/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let data;
+    import { AppShell } from "@skeletonlabs/skeleton";
+    import NavBar from "$lib/components/NavBar.svelte";
+
+    $: user = data?.user
+</script>
+
+<AppShell>
+	<svelte:fragment slot="header">
+		<NavBar {user} />
+	</svelte:fragment>
+	<slot />
+</AppShell>
